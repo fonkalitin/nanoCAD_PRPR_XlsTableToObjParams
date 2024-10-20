b976398ce9df35ecd8f74e16cdd9aa4e2fbd9f69
fonkalitin <fonkalitin@gmail.com>
2024-10-20 17:29:42 +0600

    Добавьте файлы проекта.

---
+++ Code/ObjectForInsert.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Multicad.Geometry;
+using Multicad;
+using Multicad.Objects;
+using Multicad.DatabaseServices;
+using Multicad.Symbols;
+
+namespace NCadCustom.Code
+{
+    internal class ObjectForInsert
+    {
+        /// <summary>
+        /// Парсинг строки эксель в объект который в дальнейшем помещается на чертеж.
+        /// Завязано на номера столбцов
+        /// </summary>
+        /// <param name="sst"></param>
+        /// <param name="headers"> названия шапок из эксель</param>
+        /// <param name="objectData">строка эксель</param>
+        internal ObjectForInsert(SharedStringTable sst, List<string> headers, Row objectData)
+        {
+            CreateObject(sst, headers, objectData);
+        }
+        internal Dictionary<string, string> objParams { get; set; } // Словарем определен тип данных пары значений - Строка, Строка
+        private void CreateObject(SharedStringTable sst, List<string> headers, Row objectData)
+        {
+            IEnumerable<Cell> cells = objectData.Elements<Cell>();
+            objParams = new Dictionary<string, string>();
+
+            Cell cl = cells.ElementAt(0);
+            if (cl.DataType != null && cl.DataType == CellValues.SharedString)
+            {
+                int ssid = int.Parse(cl.CellValue.Text);
+                string cellValue = sst.ChildElements[ssid].InnerText;
+
+            }
+
+           
+            // начинаются параметры для объектов БД
+            for (int iCol = 1; iCol < cells.Count(); iCol++)
+            {
+
+                    if (cells.ElementAt(iCol).DataType != null && cells.ElementAt(iCol).DataType == CellValues.SharedString)
+                    {
+                        int ssid = int.Parse(cells.ElementAt(iCol).CellValue.Text);
+                        string cellValue = sst.ChildElements[ssid].InnerText; // Извлечение текстового значения ячейки тблицы
+                        
+                        objParams.Add(headers[iCol], cellValue);
+                    }
+
+            }
+        }
+
+        /// <summary>
+        /// Выбор объекта на чертеже и запись в него параметров из таблицы
+        /// </summary>
+        internal void writeParamsToObj(McDbEntity McEntityObj)
+        {
+            List<ExValue> paramsToChange = new List<ExValue>();
+
+ 
+
+            foreach (KeyValuePair<string, string> paramsPair in objParams)
+                    {
+                        paramsToChange.Add(new ExValue(paramsPair.Key, paramsPair.Value)); // Формирование списка - пары строковых значений "ИмяПараметра-ЗначениеПараметра"
+                        McEntityObj.ObjectProperties[paramsPair.Key] = paramsPair.Value; // Непосредственно операция вставки в объект пары "Имя параметра"+""
+                    }
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using HostMgd.ApplicationServices;
+using Multicad;
+using Multicad.Objects;
+using Multicad.Symbols;
+using Multicad.Runtime;
+using Multicad.DatabaseServices;
+using DocumentFormat.OpenXml.Spreadsheet;
+using NCadCustom.Code;
+
+using App = HostMgd.ApplicationServices;
+using Db = Teigha.DatabaseServices;
+using Ed = HostMgd.EditorInput;
+using System.Runtime.Intrinsics.Arm;
+using Rtm = Teigha.Runtime;
+using System.Windows.Forms;
+
+namespace NCadCustom
+{
+    public class Commands : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            App.DocumentCollection dm = App.Application.DocumentManager;
+            Ed.Editor ed = dm.MdiActiveDocument.Editor;
+            string msg = "PRPR_objxldata - импорт данных из файла внешней таблицы Params.xlsx";
+            ed.WriteMessage(msg);
+        }
+        public void Terminate()
+        {
+        }
+
+        static readonly string[] excelExtentions = { ".xlsx", ".xls", ".xlsb", ".xlsm" };
+
+        /// <summary>
+        /// Импорт в объекты СПДС на чертеже параметров из таблицы эксель
+        /// Параметры объектов находятся в эксель
+        /// </summary>
+        /// 
+
+        
+        [Rtm.CommandMethod("PRPR_objxldata", Rtm.CommandFlags.Session)]
+        public static void MainCreateObjBySpreadSheet()
+        {
+            InputJig jig = new InputJig();
+            HostMgd.EditorInput.Editor ed = HostMgd.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            App.Document doc = App.Application.DocumentManager.MdiActiveDocument;
+
+            //string paramsFilePath = jig.GetText("Укажите полный путь до файла параметров(Excel)", false);
+            //paramsFilePath = paramsFilePath.Trim('"').ToLower();
+
+            string dwgName = doc.Name; // метод получения полного пути и имени текущего dwg-файла
+            int pos = dwgName.LastIndexOf("\\"); // позиция последнего слеша в полном пути до файла
+            string dwgPath = dwgName.Remove(pos, dwgName.Length - pos); // Путь до dwg файла (без имени файла)
+            string paramsFilePath = dwgPath + "\\Params.xlsx"; // путь до файла параметров (Excel)
+
+            if (!File.Exists(paramsFilePath))
+            {
+                ed.WriteMessage("Выбран не существующий путь! Программа завершена.");
+                return;
+            }
+
+            if (!excelExtentions.Contains(Path.GetExtension(paramsFilePath)))
+            {
+                ed.WriteMessage("Выбран не Excel файл! Программа завершена.");
+                return;
+            }
+
+            try
+            {
+                ShWorker shWorker = new ShWorker(paramsFilePath);
+                List<Row> dataRows = shWorker.dataRows;
+
+                List<string> headers = shWorker.GetHeaders(shWorker.sst, dataRows.ElementAt(0));
+                List<ObjectForInsert> objs = new List<ObjectForInsert>();
+
+                McObjectId[] idObjSelected = McObjectManager.SelectObjects("Выберите объекты: ");
+
+                // за исключением шапки - остальное строки с данными содержат параметры объекта.
+                Row rw = new Row();
+                for (int iRow = 1; iRow < dataRows.Count; iRow++)
+                {
+                    rw = dataRows[iRow];
+                    ObjectForInsert oneObject = new ObjectForInsert(shWorker.sst, headers, rw);
+
+                        McObject SelectedObj = idObjSelected[iRow-1].GetObject(); // Перебор циклом всех выбранных объектов и построчная запись параметров
+
+                        if (SelectedObj is McUMarker currObject) // Можно добавить второй тип объекта - McUMarker || McParametricObject
+                        {
+                            oneObject.writeParamsToObj(currObject.DbEntity); // Вызов метода выполняющего непосредственно вставку параметров в объект
+                        }
+                }
+
+
+                ed.WriteMessage($"Обработано позиций: {dataRows.Count - 1}");
+            }
+            catch (Exception e)
+            {
+                ed.WriteMessage($"Ошибка : {e}");
+            }
+        }
+    }
+}
